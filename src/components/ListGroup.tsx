242945bd122eba9f85f4a1b9b47b90e056d6782b
Yunjun Wang <yunjun.wangirl@gmail.com>
2025-06-20 11:40:15 +0100

    Handling events: added example of inline event handling and comments

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -21,10 +21,6 @@ function ListGroup() {
   /**
    * This built-in function useState is called a Hook.
    * We can give it an initial value.
-   */
-  const [selectedIndex, setSelectedIndex] = useState(-1);
-  const [name, setName] = useState("");
-  /*
    * The useState returns an array that has 2 elements:
    * const arr = useState(-1);
    * arr[0] is the variable
@@ -35,7 +31,32 @@ function ListGroup() {
    *
    * We can create mulitple states for the same component in the same way.
    */
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [name, setName] = useState("");
 
+  /**
+   * We can move event handling logic out of the JSX markup
+   * and create it separately here.
+   *
+   * TypeScript requires us to specify
+   * the type of function parameters explicity for type safety.
+   *
+   * To know it is coming from TypeScript, hover over mouse on the parameter,
+   * and will get this error message:
+   * Parameter 'event' implicitly has an 'any' type.ts(7006)
+   * (parameter) event: any
+   * : MouseEvent
+   *
+   * @param event — : MouseEvent
+   *
+   * When we have inline event handler code, React will be able to know
+   * which type of event it is handling;
+   * however, when we create an event handler function out of the FSX markup,
+   * TypeScript can't interpret the code to know what is the event type
+   * that is expecting.
+   *
+   * @param event: MouseEvent
+   */
   const handleBtnClick = (event: MouseEvent) => {
     /**
      * We will get the React SythenticBaseEvent here
@@ -70,7 +91,17 @@ function ListGroup() {
         className="btn btn-primary"
         onClick={handleBtnClick}
       >
-        Base Button
+        Base Button 1
+      </button>
+
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={(event) => {
+          console.log(event);
+        }}
+      >
+        Base Button 2
       </button>
     </>
   );
